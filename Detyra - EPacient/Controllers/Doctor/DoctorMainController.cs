0732ab0da8256592088bf1db917dbf8756db2c03
Gledis Ajazi <gledisajazi@yahoo.com>
2019-01-21 22:44:32 +0100

    Doctor Main Panel

---
+++ Detyra - EPacient/Controllers/Doctor/DoctorMainController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Detyra___EPacient.Helpers;
+using Detyra___EPacient.Views.Doctor;
+
+namespace Detyra___EPacient.Controllers.Doctor{
+    class DoctorMainController {
+        private DoctorMainPanel view;
+
+        public DoctorMainController(DoctorMainPanel view) {
+            this.view = view;
+        }
+
+        /**
+         * Handler for menu button click.
+         * Redirects user to one of the operator panels.
+         */
+
+        public void handleMenuButtonClick(String eventOrigin) {
+            switch (eventOrigin) {
+                case DoctorMainPanel.TIME_TABLES_DOC_BTN:
+                    Panels.switchPanels(this.view.Panel, this.view.TimeTablesDocPanel.Panel);
+                    break;
+                case DoctorMainPanel.RESERVATIONS_DOC_BTN:
+                    Panels.switchPanels(this.view.Panel, this.view.ReservationsDocPanel.Panel);
+                    break;
+                case DoctorMainPanel.PRESCRIPTION_BTN:
+                    Panels.switchPanels(this.view.Panel, this.view.PrescriptionPanel.Panel);
+                    break;
+                case DoctorMainPanel.LOG_OUT_BTN:
+                    Panels.switchPanels(this.view.Panel, this.view.LogInPanel.Panel);
+                    break;
+            }
+        }
+    }
+}
---
+++ Detyra - EPacient/Views/Doctor/TimeTablesDoc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Detyra___EPacient.Constants;
+using Detyra___EPacient.Styles;
+using Detyra___EPacient.Views.Common;
+
+namespace Detyra___EPacient.Views.Doctor {
+    class TimeTablesDoc {
+        public Panel PreviousPanel { get; set; }
+        public Panel Panel { get; set; }
+
+        private NavigationBar header;
+
+        public TimeTablesDoc(Panel previousPanel) {
+            // Init previous panel
+            this.PreviousPanel = previousPanel;
+
+            // Init panel
+            this.Panel = new Panel();
+            this.Panel.AutoSize = true;
+            this.Panel.Location = new Point(0, 0);
+            this.Panel.Name = "timetablesdocMainPanel";
+            this.Panel.Size = new Size(Dimensions.PANEL_WIDTH, Dimensions.PANEL_HEIGHT);
+            this.Panel.TabIndex = 0;
+            this.Panel.BackColor = Colors.WHITE;
+            this.Panel.Visible = false;
+
+            // Init header
+            this.header = new NavigationBar(
+                Colors.BAHAMA_BLUE,
+                "Oraret",
+                this.Panel,
+                this.PreviousPanel,
+                "../../Resources/clock.png"
+            );
+            this.Panel.Controls.Add(this.header.Panel);
+        }
+    }
+}
